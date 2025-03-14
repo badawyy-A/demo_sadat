7e2606658064ce12bc62b5badfd0fcb7d7047760
badawii21 <abdalrahman.badawyy@gmail.com>
2025-03-14 18:10:56 +0000

    cv_modules version 1

---
+++ backend/cv_modules/fitness_tests_age_9_18.py
@@ -0,0 +1,354 @@
+
+
+from pose_model import process_video_pose_estimation
+
+#Use a Savgol filter to smooth the data.
+from scipy.signal import savgol_filter
+
+# (Push-ups)
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter, find_peaks
+
+# done
+class PushupAnalyzer:
+    def __init__(self, video_path, window_length=5, poly_order=3):
+        
+        try:
+            self.df, self.FPS = process_video_pose_estimation(video_path)
+            if self.df.empty:
+                print("⚠️ Warning: The DataFrame is empty! Check process_video_pose_estimation.")
+        except Exception as e:
+            print(f"Error processing video: {e}")
+        
+        self.window_length = window_length
+        self.poly_order = poly_order
+        self.keypoints = [col for col in self.df.columns if col.startswith('kp_')]
+        self.peaks = None
+        self.valleys = None
+        self.initialize_columns()
+
+        self.reps = 0
+
+    def initialize_columns(self):
+        self.df['pushup_stage'] = 'In-Process'
+        self.df['VA_Validation'] = "NF"
+        self.df['pushup_duration'] = 0.0
+        self.df['frames_count'] = 0.0
+        self.df['pushup_speed'] = "NF"
+        self.df['pushup_cycle_duration'] = 0.0
+        self.df['pushup_cycle_speed'] = "NF"
+
+    def apply_smoothing(self):
+        for keypoint in self.keypoints:
+            self.df[f"{keypoint}_smooth"] = savgol_filter(self.df[keypoint], window_length=self.window_length, polyorder=self.poly_order)
+
+    @staticmethod
+    def calculate_angle(a, b, c):
+        a, b, c = np.array(a), np.array(b), np.array(c)
+        radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+        angle = np.abs(radians * 180.0 / np.pi)
+        return 360 - angle if angle > 180 else angle
+
+    def calculate_angles(self):
+        self.df['elbow_angle'] = self.df.apply(lambda row: self.calculate_angle(
+            [row['kp_x5_smooth'], row['kp_y5_smooth']],
+            [row['kp_x7_smooth'], row['kp_y7_smooth']],
+            [row['kp_x9_smooth'], row['kp_y9_smooth']]
+        ), axis=1)
+        
+        self.df['hip_Alignment_angle'] = self.df.apply(lambda row: self.calculate_angle(
+            [row['kp_x5_smooth'], row['kp_y5_smooth']],
+            [row['kp_x11_smooth'], row['kp_y11_smooth']],
+            [row['kp_x13_smooth'], row['kp_y13_smooth']]
+        ), axis=1)
+
+    def detect_up_down_positions(self):
+        self.peaks, _ = find_peaks(self.df['elbow_angle'], prominence=7)
+        self.valleys, _ = find_peaks(-self.df['elbow_angle'], prominence=20)
+        
+        frames_diff_peak_valley = self.peaks - self.valleys
+        if  self.valleys[0] < self.peaks[0] and self.valleys[0] > (frames_diff_peak_valley.sum() / len(frames_diff_peak_valley)):
+            self.peaks = np.insert(self.peaks, 0, 0)
+        
+        self.df.loc[self.peaks, 'pushup_stage'] = 'Up'
+        self.df.loc[self.valleys, 'pushup_stage'] = 'Down'
+
+    def plot(self):
+        """Visualize detected peaks and valleys."""
+        plt.figure(figsize=(10, 6))
+        plt.plot(self.df['elbow_angle'], label='Elbow Angle')
+        plt.plot(self.peaks, self.df['elbow_angle'][self.peaks], "x", markersize=10, label="Peaks")
+        plt.plot(self.valleys, self.df['elbow_angle'][self.valleys], "o", markersize=10, label="Valleys")
+        plt.xlabel("Frame")
+        plt.ylabel("Angle (degrees)")
+        plt.title("Peaks of Elbow Angle Over Time")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+        
+        
+    def check_performance(self, check_type):
+        if check_type == "elbow":
+            threshold_depth = (self.df['elbow_angle'].max() - self.df['elbow_angle'].min()) * 0.4
+            max_valid_angle = self.df['elbow_angle'].max() - threshold_depth
+
+            self.df["VA_Validation"] = self.df.apply(
+                lambda row: "good depth" if row["pushup_stage"] == "Down" and row['elbow_angle'] <= max_valid_angle
+                else "poor depth" if row["pushup_stage"] == "Down" else "NF",
+                axis=1
+            )
+        elif check_type == 'hip':
+            self.df['Core_feedback'] = self.df['hip_Alignment_angle'].apply(
+                lambda x: 'Core engaged' if 160 <= x <= 180 else ('Core not engaged' if x < 160 else None)
+            )
+
+    def calculate_durations(self):
+        prev_stage, start_frame = None, None
+        durations, frames = [], []
+
+        for i, row in self.df.iterrows():
+            current_stage, frame = row['pushup_stage'], row['frame_idx']
+            if current_stage == "In-Process":
+                continue
+
+            if prev_stage is None:
+                prev_stage, start_frame = current_stage, frame
+                continue
+
+            if current_stage != prev_stage:
+                frames_count = frame - start_frame
+                duration = frames_count / self.FPS
+                frames.append((i, frames_count))
+                durations.append((i, duration))
+                start_frame, prev_stage = frame, current_stage
+
+        for i, duration in durations:
+            self.df.at[i, 'pushup_duration'] = duration
+            self.df.at[i, 'pushup_speed'] = "Too Slow" if duration > 2 else "Too Fast" if duration < 1 else "Optimal Speed"
+        
+        for i, frames_count in frames:
+            self.df.at[i, 'frames_count'] = frames_count
+
+    def pushup_cycle_speed(self):
+        pushup_cycles, start_frame_idx, up_to_down_frames, prev_stage = [], None, None, None
+
+        for i, row in self.df.iterrows():
+            current_stage, frames_count, frame_idx = row['pushup_stage'], row['frames_count'], row['frame_idx']
+            if current_stage == "In-Process":
+                continue
+
+            if prev_stage is None:
+                prev_stage = current_stage
+                continue
+
+            if prev_stage == "Up" and current_stage == "Down":
+                start_frame_idx = frame_idx
+                up_to_down_frames = frames_count  # Store frames from Up to Down
+
+            elif prev_stage == "Down" and current_stage == "Up" and start_frame_idx is not None:
+                down_to_up_frames = frames_count  # Frames from Down to Up
+                total_cycle_frames = up_to_down_frames + down_to_up_frames  # Sum both 
+                cycle_duration = (total_cycle_frames * 2) / self.FPS
+                pushup_cycles.append((i, cycle_duration))
+                start_frame_idx = None
+
+            prev_stage = current_stage
+
+        for i, duration in pushup_cycles:
+            self.df.at[i, 'pushup_cycle_duration'] = duration
+            self.df.at[i, 'pushup_cycle_speed'] = "Too Slow" if duration > 4 else "Too Fast" if duration < 2 else "Optimal Speed"
+
+    def process(self):
+        self.apply_smoothing()
+        self.calculate_angles()
+        self.detect_up_down_positions()
+        self.plot()
+        self.check_performance("elbow")
+        self.calculate_durations()
+        self.check_performance("hip")
+        self.pushup_cycle_speed()  
+        self.reps = len(self.df[self.df['pushup_cycle_duration'] != 0])
+        return self.reps , self.df['pushup_cycle_duration'].sum().round(2)
+
+# Usage
+"""
+video_path = "pushups/Mod_Push Ups.mp4"
+pushup_analyzer = PushupAnalyzer(video_path)
+reps, duration = pushup_analyzer.process()
+print(reps, duration)
+"""
+# (Curl-ups)
+
+# done
+class CurlUpsTest:
+    def __init__(self, video_path, FPS=30):
+
+        try:
+            self.df, self.FPS = process_video_pose_estimation(video_path)
+            if self.df.empty:
+                print("⚠️ Warning: The DataFrame is empty! Check process_video_pose_estimation.")
+        except Exception as e:
+            print(f"Error processing video: {e}")
+        
+        self.df['current_stage'] = 'In-Process'
+        self.df['frames_count'] = 0.0
+        self.df['duration'] = 0.0
+        self.df['cycle_duration'] = 0.0
+        self.peaks = None
+        self.valleys = None
+
+    @staticmethod
+    def determine_closer_side(row, left_kp_x_col, right_kp_x_col):
+        left_x = row[left_kp_x_col]
+        right_x = row[right_kp_x_col]
+        
+        if pd.isna(left_x) or pd.isna(right_x):
+            return "ambiguous"
+        if left_x < right_x:
+            return "right"
+        elif right_x < left_x:
+            return "left"
+        else:
+            return "ambiguous"
+
+    def preprocess(self):
+        self.df['closer_side'] = self.df.apply(
+            self.determine_closer_side, axis=1, left_kp_x_col='kp_x3', right_kp_x_col='kp_x4'
+        )
+        
+        keypoints = [col for col in self.df.columns if col.startswith('kp_')]
+        for keypoint in keypoints:
+            self.df[f"{keypoint}_smooth"] = savgol_filter(self.df[keypoint], window_length=5, polyorder=3)
+
+    @staticmethod
+    def calculate_angle(a, b, c):
+        a = np.array(a)
+        b = np.array(b)
+        c = np.array(c)
+        radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+        angle = np.abs(radians*180.0/np.pi)
+        if angle >180.0:
+            angle = 360-angle
+        return angle
+
+    def calculate_angles(self):
+        side = self.df['closer_side'].max()
+        if side == "right":
+            self.df['hip_Alignment_angle'] = self.df.apply(lambda row: self.calculate_angle(
+                [row['kp_x5_smooth'], row['kp_y5_smooth']],
+                [row['kp_x11_smooth'], row['kp_y11_smooth']],
+                [row['kp_x13_smooth'], row['kp_y13_smooth']]
+            ), axis=1)
+        else:
+            self.df['hip_Alignment_angle'] = self.df.apply(lambda row: self.calculate_angle(
+                [row['kp_x6_smooth'], row['kp_y6_smooth']],
+                [row['kp_x12_smooth'], row['kp_y12_smooth']],
+                [row['kp_x14_smooth'], row['kp_y14_smooth']]
+            ), axis=1)
+
+    def detect_up_down_positions(self):
+        self.peaks, _ = find_peaks(self.df['hip_Alignment_angle'], prominence=7)
+        self.valleys, _ = find_peaks(-self.df['hip_Alignment_angle'], prominence=20)
+        
+        min_len = min(len(self.peaks), len(self.valleys))
+        peaks_e, valleys_e = self.peaks[:min_len], self.valleys[:min_len]
+
+        
+        frames_diff_peak_valley = peaks_e - valleys_e
+        
+        if  self.valleys[0] < self.peaks[0] and self.valleys[0] >= (frames_diff_peak_valley.sum() / len(frames_diff_peak_valley)):
+            self.peaks = np.insert(self.peaks, 0, 0)
+        
+        self.df.loc[self.peaks, 'current_stage'] = 'Down'
+        self.df.loc[self.valleys, 'current_stage'] = 'Up'
+        
+    def plot(self):
+        plt.figure(figsize=(10, 6))
+        plt.plot(self.df['hip_Alignment_angle'], label='hip_Alignment_angle')
+        plt.plot(self.peaks, self.df['hip_Alignment_angle'][self.peaks], "x", markersize=10, label="Peaks")
+        plt.plot(self.valleys, self.df['hip_Alignment_angle'][self.valleys], "o", markersize=10, label="Valleys")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    def duration_speed_comment(self):
+        prev_stage, start_frame, durations, frames = None, None, [], []
+        
+        for i, row in self.df.iterrows():
+            current_stage, frame = row['current_stage'], row['frame_idx']
+            
+            if current_stage == "In-Process":
+                continue
+            
+            if prev_stage is None:
+                prev_stage, start_frame = current_stage, frame
+                continue
+            
+            if current_stage != prev_stage and (current_stage != "in-progress") and prev_stage != "in-progress":
+                frames_count = frame - start_frame
+                duration = frames_count / self.FPS
+                frames.append((i, frames_count))
+                durations.append((i, duration))
+                
+                start_frame = frame  # Reset start frame for new stage
+                prev_stage = current_stage
+        
+        for i, frames_count in frames:
+            self.df.at[i, 'frames_count'] = frames_count
+        
+        for i, duration in durations:
+            self.df.at[i, 'duration'] = duration
+
+    def pushup_cycle_speed(self):
+        cycles, start_frame_idx, up_to_down_frames, prev_stage = [], None, None, None
+        
+        for i, row in self.df.iterrows():
+            current_stage = row['current_stage']
+            frames_count = row['frames_count']  # Frames between current and previous stage
+            frame_idx = row['frame_idx']            
+            
+            if current_stage == "In-Process":
+                continue
+            
+            if prev_stage is None:
+                prev_stage = current_stage
+                continue
+            
+            # Detect Up → Down transition (start of cycle)
+            if prev_stage == "Down" and current_stage == "Up":
+                start_frame_idx = frame_idx
+                down_to_up_frames = frames_count  # Store frames from Up to Down
+
+            # Detect Down → Up transition (end of cycle)
+            elif prev_stage == "Up" and current_stage == "Down" and start_frame_idx is not None:
+                up_to_down_frames = frames_count  # Frames from Down to Up
+                total_cycle_frames = up_to_down_frames + down_to_up_frames  # Sum both transitions
+                cycle_duration = total_cycle_frames / self.FPS
+                cycles.append((i, cycle_duration))
+                start_frame_idx = None
+
+            prev_stage = current_stage
+        
+        for i, duration in cycles:
+            self.df.at[i, 'cycle_duration'] = duration
+
+    def process(self):
+        self.preprocess()
+        self.calculate_angles()
+        self.detect_up_down_positions()
+        self.plot()
+        self.duration_speed_comment()
+        self.pushup_cycle_speed()
+        return len(self.df[self.df['cycle_duration'] != 0]), self.df['cycle_duration'].sum().round(2)
+
+# Usage example:
+"""video_path = "curl/curl_g.mp4"
+curlups = CurlUpsTest(video_path)
+reps, total_duration = curlups.process()
+print(reps, total_duration)
+"""
+
+# speed test (distance & duration)
+
---
+++ backend/cv_modules/pose_model.py
@@ -0,0 +1,144 @@
+import pandas as pd
+import cv2
+from ultralytics import YOLO
+
+
+
+# done
+def create_pose_dataframe(results, frame_idx=0):
+    """
+    Creates a Pandas DataFrame from YOLOv8 pose estimation tracking results.
+
+    Args:
+        results: Output from YOLOv8 pose estimation model's track function.
+                 Assumes it's a list of objects where each object contains
+                 detection information (boxes, keypoints, track_id).
+        frame_idx (int, optional): Frame index. Defaults to 0.
+
+    Returns:
+        pandas.DataFrame: DataFrame containing frame index, person ID,
+                          bounding box, and keypoint coordinates.
+    """
+
+    frame_indices = []
+    player_ids = []
+    x1_list = []
+    y1_list = []
+    x2_list = []
+    y2_list = []
+    kp_x_lists = [[] for _ in range(17)]
+    kp_y_lists = [[] for _ in range(17)]
+
+    if results and results.boxes:
+        for res in results:
+            if res.boxes and res.keypoints and res.boxes.cls[0] == 0:
+                box = res.boxes.xyxy[0]
+                keypoints = res.keypoints.xy[0]
+                track_id = res.boxes.id.cpu().numpy()[0] if res.boxes.id is not None else -1
+
+                frame_indices.append(frame_idx)
+                player_ids.append(track_id)
+                x1_list.append(box[0].item())
+                y1_list.append(box[1].item())
+                x2_list.append(box[2].item())
+                y2_list.append(box[3].item())
+
+                for i in range(17):
+                    kp_x_lists[i].append(keypoints[i][0].item())
+                    kp_y_lists[i].append(keypoints[i][1].item())
+    else:
+        print(f"No person detected in frame {frame_idx} or empty results.")
+        return pd.DataFrame()
+
+    data = {
+        "frame_idx": frame_indices,
+        "player_id": player_ids,
+        "x1": x1_list,
+        "y1": y1_list,
+        "x2": x2_list,
+        "y2": y2_list,
+    }
+
+    for i in range(17):
+        data[f"kp_x{i}"] = kp_x_lists[i]
+        data[f"kp_y{i}"] = kp_y_lists[i]
+
+    df = pd.DataFrame(data)
+    return df
+
+
+# done
+def process_video_pose_estimation(video_path, model_path="yolo11x-pose.pt"):
+    """
+    Processes a video to perform pose estimation and tracking, and saves/returns results as a Pandas DataFrame.
+
+    Args:
+        video_path (str): Path to the input video file.
+        output_csv_path (str, optional): Path to save the output CSV file. Defaults to "pose_results.csv".
+
+    Returns:
+        pandas.DataFrame: DataFrame containing pose estimation results for the video.
+    """
+    model = YOLO(model_path)
+    
+    
+    try:
+        cap = cv2.VideoCapture(video_path)
+        if not cap.isOpened():
+            raise IOError("Error: Cannot open video file.")
+    except IOError as e:
+        print(e)
+        return pd.DataFrame() # Return empty DataFrame in case of error
+    
+    
+    if not cap.isOpened():
+        print("Error: Cannot open video file.")
+        return pd.DataFrame()
+
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    frame_index = 0
+    all_frames_df = []
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        try:
+            results = model.track(frame, conf=0.1, iou = 0 , persist=True, classes=[0], verbose=True, pose=True)
+        except Exception as e:
+            print(f"Error processing frame {frame_index}: {e}")
+            continue
+
+        df_current_frame = create_pose_dataframe(results[0], frame_index)
+
+        if not df_current_frame.empty:
+            all_frames_df.append(df_current_frame)
+
+        frame_index += 1
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    if all_frames_df:
+        final_df = pd.concat(all_frames_df, ignore_index=True)
+        return final_df, fps
+    else:
+        print("No pose data extracted from the video.")
+        return pd.DataFrame(), fps
+
+      # <--- REPLACE THIS WITH DESIRED OUTPUT CSV PATH
+
+"""video_file = "plate-tapping/plate_21.mp4"  # <--- REPLACE THIS WITH YOUR VIDEO PATH
+
+pose_df, FPS = process_video_pose_estimation(video_file)
+
+print(pose_df.info())
+print(pose_df.columns)
+"""
+
+
+
