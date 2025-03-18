f8a5cb9eb05d2db7d1fb934d8d0740a4807437ac
ahmed.shafiq <ahmed.shafiq@fixed.global>
2025-03-18 20:58:06 +0000

    sdat 1

--- backend/cv_modules/process.py
+++ backend/cv_modules/process.py
@@ -4,10 +4,11 @@ import json
 import os
 
 
-def process_age5_8(plate_video_path, flamingo_video_path,output_path, age):
+def process_age5_8(plate_video_path, flamingo_video_path ,output_path, age):
     plate_test = PlateTappingTest(plate_video_path)
     flamingo_test = FlamingoBalance_Score(flamingo_video_path)
     plate_result = plate_test.run_test()
+
     print("before plate_test: ",plate_result)
     print("after: ", float(plate_result))
     age_range_result = {  
@@ -21,14 +22,16 @@ def process_age5_8(plate_video_path, flamingo_video_path,output_path, age):
     return age_range_result
 
 
-def process_age9_18(push_ups_path , curl_ups_path, output_path, age):
+def process_age9_18(push_ups_path , curl_ups_path, run_walk_video_path , speed_video_path, output_path, age):
     push_ups_test = PushupAnalyzer(push_ups_path)
     curl_ups_test = CurlUpsTest(curl_ups_path)
-    
+    run_walk_600 = Running_Score(run_walk_video_path)
+    speed_50_dash = Running_Score(speed_video_path)
     age_range_result = {  
         "push_ups": {"age": age, "repetitions": int(push_ups_test.process())},
-        "curl_ups" : {"age": age, "repetitions": int(curl_ups_test.process())}
-        #"run_600m" : {"age": age, "total_time_ms": flamingo_test.balance_loss()},
+        "curl_ups" : {"age": age, "repetitions": int(curl_ups_test.process())},
+        "run_600m" : {"age": age, "total_time_ms": run_walk_600.process_video()},
+        "dash_50m" : {"age": age, "total_time_ms": speed_50_dash.process_video()},
     }
     # Save to a JSON file
     with open(os.path.join(output_path , 'scores_results_age9_18.json'), "w") as json_file:
